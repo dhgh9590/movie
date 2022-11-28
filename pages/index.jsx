d9900caf866cec44662f287517bb9cca09a8e709
hoseol OH <dhgh9590@naver.com>
2022-11-28 15:14:30 +0900

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+/* prop 타입체크 */
+// import PropTypes from 'prop-types';
+
+/* 폰트어썸 */
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+const Home = () => {
+  return <div>메인</div>;
+};
+
+/*
+Index.propTypes = {
+  prop이름: PropTypes.array,
+};
+*/
+
+export default Home;
---
+++ src/constants/api.js
@@ -0,0 +1,1 @@
+export const BASE_URL = 'http://158.247.214.79/api';
